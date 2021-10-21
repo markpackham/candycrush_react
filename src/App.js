bc9b1af2a8f95e4ff1f15c222cf9665f5f36c9e2
mark <markpackham1@gmail.com>
2021-10-21 16:29:10 +0100

    check for columns of 4
    https://youtu.be/PBrEq9Wd6_U?t=2239

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfFour.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfFour.forEach(
+          (square) => (currentColorArrangement[square] = "")
+        );
+      }
+    }
+  };
+
   const checkForColumnOfThree = () => {
     for (let i = 0; i < 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
@@ -39,11 +56,12 @@ const App = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour();
       checkForColumnOfThree();
       setCurrentColorArrangement([...currentColorArrangement]);
     }, 100);
     return () => clearInterval(timer);
-  }, [checkForColumnOfThree, currentColorArrangement]);
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
 
   return (
     <div className="app">
