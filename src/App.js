9615737614a820d8376a542c177b8e8db724f473
mark <markpackham1@gmail.com>
2021-10-21 11:41:43 +0100

    output board
    https://youtu.be/PBrEq9Wd6_U?t=1510

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import { useState, useEffect } from "react";
+
 const width = 8;
 const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const checkForColumnOfThree = () => {};
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomNumFrom0to5 = Math.floor(Math.random() * candyColors.length);
+      const randomColor = candyColors[randomNumFrom0to5];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            alt={candyColor}
+            key={index}
+            style={{ backgroundColor: candyColor }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
