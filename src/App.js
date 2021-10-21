a7631d549755aac82b54373280c99e4bb20d46a7
mark <markpackham1@gmail.com>
2021-10-21 16:35:46 +0100

    check row of three
    https://youtu.be/PBrEq9Wd6_U?t=2424

--- src/App.js
+++ src/App.js
@@ -40,6 +40,27 @@ const App = () => {
     }
   };
 
+  const checkForRowsOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+
+      // Don't waste time checking rows that are the last two on the right side
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfThree.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -58,10 +79,16 @@ const App = () => {
     const timer = setInterval(() => {
       checkForColumnOfFour();
       checkForColumnOfThree();
+      checkForRowsOfThree();
       setCurrentColorArrangement([...currentColorArrangement]);
     }, 100);
     return () => clearInterval(timer);
-  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
+  }, [
+    checkForColumnOfFour,
+    checkForColumnOfThree,
+    checkForRowsOfThree,
+    currentColorArrangement,
+  ]);
 
   return (
     <div className="app">
